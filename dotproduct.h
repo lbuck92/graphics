afeeeec631fd3d133643deb902eb669756858388
lbuck92 <lauren.e.buck.1@vanderbilt.edu>
2019-10-02 13:38:17 -0500

    dotproduct finished

--- dotproduct.h
+++ dotproduct.h
@@ -8,10 +8,23 @@ class DotProduct {
   public:
       int product;
       void dot_product2D (vector<int> v1, vector<int> v2);
+      void dot_product3D (vector<int> v1, vector<int> v2);
+      void dot_productND (vector<int> v1, vector<int> v2);
 };
 
 void DotProduct::dot_product2D(vector<int> v1, vector<int> v2){
   product = ((v1[0]*v2[0]) + (v1[1]*v2[1]));
+}
+
+void DotProduct::dot_product3D(vector<int> v1, vector<int> v2){
+  product = ((v1[0]*v2[0]) + (v1[1]*v2[1]) + (v1[2]*v2[2]));
+}
+
+void DotProduct::dot_productND(vector<int> v1, vector<int> v2){
+  product = 0;
+  for (int i = 0; i < v1.size(); i++) {
+    product += (v1[i]*v2[i]);
+  }
 }
 
 #endif // DOTPRODUCT_H
