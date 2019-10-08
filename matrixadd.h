29a71c1d9ae99dfccd4ee703e7f9bb58e9fffaf2
lbuck92 <lauren.e.buck.1@vanderbilt.edu>
2019-10-08 09:02:17 -0500

    addsub

--- matrixadd.h
+++ matrixadd.h
@@ -2,12 +2,15 @@
 #define MATRIXADD_H
 
 #include <vector>
+#include <stdexcept>
 using namespace std;
 
 
 class AddMatrix{
   public:
     vector<vector<float> > addMatrix(int rows, int cols, vector<vector<float> > matrix1,
+      vector<vector<float> > matrix2);
+    vector<vector<float> > subMatrix(int rows, int cols, vector<vector<float> > matrix1,
       vector<vector<float> > matrix2);
 };
 
@@ -19,11 +22,7 @@ vector<vector<float> > AddMatrix::addMatrix(int rows, int cols, vector<vector<fl
   //cannot add the matrix if the number of rows
   //doesn't equal the number of cols
   if(rows != cols){
-    for(int i = 0; i < rows; i++){
-      for(int j = 0; j < cols; j++){
-        matrix3[i][j] = 0;
-      }
-    }
+    throw std::invalid_argument("Vector dimensions not the same!");
   }else{
     for(int i = 0; i < rows; i++){
       for(int j = 0; j < cols; j++){
@@ -35,5 +34,21 @@ vector<vector<float> > AddMatrix::addMatrix(int rows, int cols, vector<vector<fl
   return matrix3;
 
 }
+
+vector<vector<float> > AddMatrix::subMatrix(int rows, int cols, vector<vector<float> > matrix1,
+  vector<vector<float> > matrix2){
+    vector<vector<float> > matrix3(rows,vector<float>(rows));
+
+    if(rows != cols){
+      throw std::invalid_argument("Vector dimensions not the same!");
+    }else{
+      for(int i = 0; i < rows; i++){
+        for(int j = 0; j < cols; j++){
+          matrix3[i][j] = matrix1[i][j] - matrix2[i][j];
+        }
+      }
+    }
+    return matrix3;
+  }
 
 #endif
